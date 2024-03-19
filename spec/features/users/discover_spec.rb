d6e67b48e7327a95ed99862fed584d2ba6d28384
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-19 14:21:14 -0600

    Add sad path testing

--- spec/features/users/discover_spec.rb
+++ spec/features/users/discover_spec.rb
@@ -21,5 +21,16 @@ RSpec.describe 'movies discover', type: :feature do
       # - a Button to Search by Movie Title
       expect(page).to have_button('Find Movies')
     end
+
+    #Sad Path Testing, Feature 1
+    it 'responds to incomplete information in the text field' do
+      visit user_discover_index_path(@user_tommy.id)
+      fill_in(:movie_title, with: "")
+      click_button("Find Movies")
+
+      expect(current_path).to eq(user_discover_index_path(@user_tommy.id))
+
+      expect(page).to have_content("Error: Try a different movie title")
+    end
   end
 end
