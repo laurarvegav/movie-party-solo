107189827fe142a53e78a610e72d8a0a78994744
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-19 18:05:07 -0600

    Create tests for instance methods on MovieService class

--- spec/models/movie_service_spec.rb
+++ spec/models/movie_service_spec.rb
@@ -2,16 +2,23 @@ require 'rails_helper'
 
 RSpec.describe MovieService do
   describe 'Instance Methods' do
-    it "gets URL, populating API records into JSON" do
-      url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc"
+    it "gets URL, populating API records into JSON", :vcr do
+      url = "https://api.themoviedb.org/3/discover/movie"
 
       parsed_movie = MovieService.new.get_url(url)
 
       expect(parsed_movie.first).to be_a(Hash)
-      expect(parsed_movie.first["name"]).to be_a(String)
-      expect(parsed_movie.first["vote"]).to be_a(Float)
-      expect(parsed_movie.first["runtime"]).to be_a(String)
-      expect(parsed_movie.first["genre"]).to be_a(String)
+      expect(parsed_movie.first[:name]).to be_a(String)
+      expect(parsed_movie.first[:vote]).to be_a(Float)
+      expect(parsed_movie.first[:runtime]).to be_a(String)
+      expect(parsed_movie.first[:genre]).to be_a(String)
+    end
+
+    it "returns the top 20 movies", :vcr do
+      parsed_movie = MovieService.new.top_movies
+
+      expect(parsed_movie.first).to be_a(Hash)
+      expect(parsed_movie.length).to eq(20)
     end
   end
 end
