af0046ac977c713ae4892f301507d67a16d1a601
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-20 17:23:18 -0600

    Test for initialize method for Movie poros

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Movie, type: :model do
-  describe 'instance methods' do
+RSpec.describe Movie do
+  it 'exists' do
+    data = {original_title: "Kung Fu Panda", vote_average: "7.9"}
+    movie_kfp = Movie.new(data)
 
+    expect(movie_kfp).to be_a(Movie)
   end
 end
