e5e00de686fe5df418f13ad8c8ffd031cf4cf085
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-20 17:22:06 -0600

    Add index action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,12 @@
 class MoviesController < ApplicationController
-
   def index
-    user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
+
     if status == 400
-      redirect_to user_discover_index_path(user.id)
+      redirect_to user_discover_index_path(@user.id)
       flash[:alert] =  "Error: Try a different movie title"
+    else
+      @facade = ServiceFacade.new(@user)
     end
   end
 end
