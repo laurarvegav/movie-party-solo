5fac697ca541cb6a36646f779991795843b9fe3b
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-20 16:52:47 -0600

    Add tests for Movies Result page, User story 2

---
+++ spec/features/users/index_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe 'Movies Results Page', type: :feature do
+  describe 'As a user' do
+    before(:each) do
+      @user_tommy = User.create!(name: 'Tommy', email: 'tommy@email.com')
+      @user_sam = User.create!(name: 'Sam', email: 'sam@email.com')
+    end
+
+    # User Story #2
+    it 'shows movie title as link to Movie Details Page, voters average and button to the Discover Page', :vcr do
+      # When I visit the discover movies page ('/users/:id/discover'),
+      visit user_discover_index_path(@user_tommy.id)
+      # and click on either the Discover Top Rated Movies button or fill out the movie title search and click the Search button,
+      click_button("Discover Top Rated Movies")
+      # I should be taken to the movies results page (`users/:user_id/movies`) where I see:
+      expect(current_path).to eq(user_movies_path(@user_tommy.id))
+
+      # - Title (As a Link to the Movie Details page (see story #3))
+      expect(page).to have_link("Damsel")
+      # - Vote Average of the movie
+      expect(page).to have_content("Damsel Vote average: 7.23")
+
+      # I should also see a button to return to the Discover Page.
+      click_button("Discover Page")
+      expect(current_path).to eq(user_discover_index_path(@user_tommy.id))
+
+      fill_in(:movie_title, with: "Kung")
+      click_button("Find Movies")
+      expect(current_path).to eq(user_movies_path(@user_tommy.id))
+      expect(page).to have_content("Kung Fu Panda 4")
+      expect(page).to have_content("Vote Average: 9.7")
+
+      expect(page).to have_button("Discover Page")
+    end
+  end
+end
