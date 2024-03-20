ffa5838e6cc8264e8a96fb53259b0324a91cbed9
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-19 20:12:02 -0600

    Complete instance methods dor MovieService

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,6 +1,6 @@
 class MovieService
   def top_movies
-    get_url("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc").take(20)
+    get_url("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc").take(20)[1][1]
   end
 
   def get_url(url)
@@ -9,8 +9,12 @@ class MovieService
   end
 
   def mov
-    Faraday.new(url: "https://api.themoviedb.org/3/discover/movie") do |faraday|
-      faraday.headers["api_key"] = "Bearer #{Rails.application.credentials.movies_api_key}"
+    Faraday.new(url: "https://api.themoviedb.org/3/dicover/movie") do |faraday|
+      faraday.params["api_key"] = Rails.application.credentials.tmbd[:key]
     end
+  end
+
+  def find_movie_by_name(string)
+    get_url("https://api.themoviedb.org/3/search/keyword?query=#{string}").take(20)[1][1]
   end
 end
