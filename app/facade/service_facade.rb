5a7a7bde61110e50e279fa36d98efd4fb91754ca
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-21 00:34:04 -0600

    Refactor

--- app/facade/service_facade.rb
+++ app/facade/service_facade.rb
@@ -1,19 +1,20 @@
 class ServiceFacade
   attr_reader :user
+              :movie_title
 
-  def initialize(user)
+  def initialize(user, movie_title=nil)
     @user = user
+    @movie_title = movie_title
   end
 
   def movies
     service = MovieService.new
 
-    if params[:movie_title]
-      json = service.find_movie_by_name(params[:movie_title])
+    if @movie_title != nil
+      json = service.find_movie_by_name(@movie_title)
     else
       json = service.top_movies
     end
-
     json[:results].map do |movie_data|
       Movie.new(movie_data)
     end
