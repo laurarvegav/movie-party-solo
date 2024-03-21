7da487f1f550901139f47b46eb531efbaeb5146a
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-21 14:36:00 -0600

    Fix routes typo

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -7,8 +7,23 @@ class ViewingPartiesController < ApplicationController
   end
 
   def create
-    @viewing_party = ViewingParty.create!(duration: params[:party_duration], date: params[:select_date], start_time: params[:start_time])
+    user = User.find(params[:user_id])
+    movie = ServiceFacade.new(user, params[:movie_id].to_i).movies
 
-    redirect_to user_movie_viewing_party_path(user_id: params[:user_id], movie_id: params[:movie_id], id: @viewing_party.id)
+    if params[:party_duration].to_i < movie.raw_runtime
+      redirect_to new_user_movie_viewing_party_path(user.id, movie.id)
+
+      flash[:alert] = "Error: Party duration needs to cover at least the movie runtime"
+
+    elsif params[:select_date] == ""
+      redirect_to new_user_movie_viewing_party_path(user.id, movie.id)
+
+      flash[:alert] = "Error: Party date can't be empty"
+
+    else
+      @viewing_party = ViewingParty.create!(duration: params[:party_duration], date: params[:select_date], start_time: params[:start_time])
+
+      redirect_to user_movie_viewing_party_path(user_id: params[:user_id], movie_id: params[:movie_id], id: @viewing_party.id)
+    end
   end
 end
