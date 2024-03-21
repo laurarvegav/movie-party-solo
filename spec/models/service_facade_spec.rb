c07398dc168347747f7f8127b79ca4d6ceb05092
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-21 01:22:51 -0600

    Refactor default movie_id to movie and value to false, case handling for id and title

--- spec/models/service_facade_spec.rb
+++ spec/models/service_facade_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe ServiceFacade do
   before do
     @user_sam = User.create!(name: 'Sam', email: 'sam@email.com')
     @facade = ServiceFacade.new(@user_sam)
+    @facade_movie = ServiceFacade.new(@user_sam, "Kung Fu Panda 4")
+    @facade_movie = ServiceFacade.new(@user_sam, 1011985)
   end
 
   it 'exists' do
@@ -12,6 +14,9 @@ RSpec.describe ServiceFacade do
 
   it 'returns movie objects', :vcr do
     expect(@facade.movies).to be_an(Array)
+    expect(@facade_movie.movies).to be_an(Array)
     expect(@facade.movies.all? {|movie| movie.class == Movie}).to eq(true)
+    expect(@facade_movie.movies.all? {|movie| movie.class == Movie}).to eq(true)
+    expect(@facade_movie.movies.first.id).to eq(1011985)
   end
 end
