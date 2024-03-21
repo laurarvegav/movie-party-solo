9fe6d19e21bfd29d8ae1ba8af0ac4fdec6840150
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-21 12:23:12 -0600

    Fix route when create party button is clicked

--- app/controllers/viewing_party_controller.rb
+++ app/controllers/viewing_party_controller.rb
@@ -7,6 +7,8 @@ class ViewingPartyController < ApplicationController
   end
 
   def create
+    viewing_party = ViewingParty.create!(duration: params[:party_duration], date: params[:select_date], start_time: params[:start_time])
 
+    redirect_to user_movie_viewing_party_path(user_id: params[:user_id], movie_id: params[:movie_id], id: viewing_party.id)
   end
 end
