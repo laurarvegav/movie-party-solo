b165117e2055215b94fdac91648528f8c64f0a02
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-21 13:43:21 -0600

    Fix typo naming routes for ViewingParties

---
+++ app/controllers/viewing_parties_controller.rb
@@ -0,0 +1,14 @@
+class ViewingPartiesController < ApplicationController
+  def new
+    @user = User.find(params[:user_id])
+    facade = ServiceFacade.new(@user, params[:movie_id].to_i)
+    @facade_movie = facade.movies
+    @guests = User.all.except(@user)
+  end
+
+  def create
+    @viewing_party = ViewingParty.create!(duration: params[:party_duration], date: params[:select_date], start_time: params[:start_time])
+
+    redirect_to user_movie_viewing_party_path(user_id: params[:user_id], movie_id: params[:movie_id], id: @viewing_party.id)
+  end
+end
