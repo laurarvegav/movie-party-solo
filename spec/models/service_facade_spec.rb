e785fd761a59eee0e660d55c6171ea0c65c64fc4
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-21 02:19:53 -0600

    Fix tests for @facade_movie_id

--- spec/models/service_facade_spec.rb
+++ spec/models/service_facade_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe ServiceFacade do
   before do
     @user_sam = User.create!(name: 'Sam', email: 'sam@email.com')
     @facade = ServiceFacade.new(@user_sam)
-    @facade_movie = ServiceFacade.new(@user_sam, "Kung Fu Panda 4")
-    @facade_movie = ServiceFacade.new(@user_sam, 1011985)
+    @facade_movie_title = ServiceFacade.new(@user_sam, "Kung Fu Panda 4")
+    @facade_movie_id = ServiceFacade.new(@user_sam, 1011985)
   end
 
   it 'exists' do
@@ -14,9 +14,11 @@ RSpec.describe ServiceFacade do
 
   it 'returns movie objects', :vcr do
     expect(@facade.movies).to be_an(Array)
-    expect(@facade_movie.movies).to be_an(Array)
+    expect(@facade_movie_title.movies).to be_an(Array)
+    expect(@facade_movie_id.movies).to be_a(Movie)
     expect(@facade.movies.all? {|movie| movie.class == Movie}).to eq(true)
-    expect(@facade_movie.movies.all? {|movie| movie.class == Movie}).to eq(true)
-    expect(@facade_movie.movies.first.id).to eq(1011985)
+    expect(@facade_movie_title.movies.all? {|movie| movie.class == Movie}).to eq(true)
+    expect(@facade_movie_title.movies.first.id).to eq(1011985)
+    expect(@facade_movie_id.movies.title).to eq("Kung Fu Panda 4")
   end
 end
