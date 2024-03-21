0c85f04aa87d0d0d3961b2aa18893ee3613b0c1d
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-21 04:19:47 -0600

    Complete Movie attributes with helper methods and cast and review methods

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -10,9 +10,41 @@ class Movie
     @id = data[:id]
     @title = data[:original_title]
     @vote = data[:vote_average].round(2)
-    # @runtime = data[:runtime]
-    # @genre = data[:genre]
-    # @id = data[:id]
-    # @summary = data[:overview]
+    @summary = data[:overview]
+    @runtime = format_runtime(data[:runtime])
+    @genre = format_genre(data[:genres])
+  end
+
+  def format_runtime(data)
+    hours = data / 60
+    minutes = data % 60
+  
+    formatted_runtime = ""
+    formatted_runtime += "#{hours}hr " if hours > 0
+    formatted_runtime += "#{minutes}min" if minutes > 0
+  
+    formatted_runtime.strip
+  end
+
+  def format_genre(data)
+    data.map do |genre|
+      genre[:name]
+    end.join(", ")
+  end
+
+  def cast(data)
+    cast = {}
+    data.each do |member|
+      cast[member[:character]] = member[:name]
+    end
+    cast
+  end
+
+  def reviews(data)
+    reviews = {}
+    data.map do |review|
+      reviews[review[:author]] = review[:content]
+    end
+    reviews
   end
 end
