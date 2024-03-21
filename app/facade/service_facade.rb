c69b23428ae3b1ac6c1ad994eb76cab369d29b3a
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-21 05:29:04 -0600

    Create methods movie_cast and movie_review

--- app/facade/service_facade.rb
+++ app/facade/service_facade.rb
@@ -16,16 +16,55 @@ class ServiceFacade
   def movies
     service = MovieService.new
 
-    if @movie_title != nil
-      json = service.find_movie_by_name(@movie_title)
-    elsif @movie_id != nil
-      movie_data = service.find_movie_by_id(@movie_id)
-      return Movie.new(movie_data)
+    if @movie_id == nil
+
+      if @movie_title != nil
+        json = service.find_movie_by_name(@movie_title)
+      else
+        json = service.top_movies
+      end
+
+      json[:results].map do |movie_data|
+        Movie.new(movie_data)
+      end
+
     else
-      json = service.top_movies
-    end
-    json[:results].map do |movie_data|
+      movie_data = service.find_movie_by_id(@movie_id)
       Movie.new(movie_data)
     end
+  end
+
+  def movie_cast
+    service = MovieService.new
+
+    json = service.find_movie_credits(@movie_id)
+
+    cast = []
+
+    10.times do |index|
+      cast_data = json[:cast][index]
+      cred = {
+        name: cast_data[:name],
+        character: cast_data[:character]
+      }
+      cast << cred
+    end
+    cast
+  end
+
+  def movie_review
+    service = MovieService.new
+
+    json = service.find_movie_reviews(@movie_id)
+
+    cast = []
+    json[:results].map do |review_data|
+      cred = {
+        author: review_data[:author],
+        content: review_data[:content]
+      }
+      cast << cred
+    end
+    cast
   end
 end
