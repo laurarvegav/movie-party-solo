e1a8d8e02aa7da499b7c9ab59d256cb9c3da6e87
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-03-22 00:51:06 -0600

    Refactor find_similar_movies method test to evaluate SimilarMovie objects array

--- spec/models/service_facade_spec.rb
+++ spec/models/service_facade_spec.rb
@@ -55,7 +55,7 @@ RSpec.describe ServiceFacade do
 
   it "returns an array of similar movies given base movie's id", :vcr do
     expect(@facade_movie_id.find_similar_movies).to be_an(Array)
-    expect(@facade_movie_id.find_similar_movies.all? {|movie| movie.class == Movie}).to eq(true)
+    expect(@facade_movie_id.find_similar_movies.all? {|movie| movie.class == SimilarMovie}).to eq(true)
     expect(@facade_movie_id.find_similar_movies.first.id).to eq(14813)
   end
 end
