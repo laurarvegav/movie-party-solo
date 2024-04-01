713e61fb63b099ea12676ba676bbcfb323a5ed3c
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-01 16:23:49 -0600

    Test: Add tests to validate presence of password_digest and for should_have_secure_password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,12 +9,21 @@ RSpec.describe User, type: :model do
     it { should_not allow_value('something somthing@something.something').for(:email) }
     it { should_not allow_value('something.something@').for(:email) }
     it { should_not allow_value('something').for(:email) }
+    it { should validate_presence_of :password_digest }
+    it { should have_secure_password }
+  end
 
+  describe "should have secure password" do
+    it "encrypts user's password" do
+      user = User.create(name: 'Meg', email: 'meg@test.com', password: 'password123', password_confirmation: 'password123')
+      
+      expect(user).to_not have_attribute(:password)
+      expect(user.password_digest).to_not eq('password123')
+    end
   end
 
   describe 'associations' do
     it { should have_many :user_parties }
     it { should have_many(:viewing_parties).through(:user_parties) }
   end
-  
 end
