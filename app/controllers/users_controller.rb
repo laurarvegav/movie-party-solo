e799cf3627815f1fc93dc9daf76d65faab39545c
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-01 17:23:26 -0600

    Feat: add log in form and log in actions for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,10 +26,21 @@ class UsersController < ApplicationController
       
    end
 
-private
+   def login_form; end
 
+   def login
+      user = User.find_by(email: params[:email])
+      if user.authenticate(params[:password])
+         flash[:success] = "Welcome, #{user.name}!"
+         redirect_to user_path(user)
+      else
+         flash[:error] = "Sorry, incorrect credentials"
+         render :login_form
+      end
+   end
+
+private
   def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-
 end
