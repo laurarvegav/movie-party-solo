6ab03690a0936b056c15f18b96c2bcc14f6d26bd
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-01 16:24:21 -0600

    Feat: Add valiadions for presence of password_digest and for should_have_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
    validates_presence_of :name, :email
    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
+   validates_presence_of :password_digest
+   has_secure_password
+
    has_many :user_parties
    has_many :viewing_parties, through: :user_parties
 end
