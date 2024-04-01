3ad2b68870eea584420ec000c930acffbf3bae39
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-01 16:26:38 -0600

    Feat: Update users controller create action with password and password_confirmation fields conditions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,13 @@ class UsersController < ApplicationController
 
    def create
       user = User.new(user_params)
+
+      if user_params[:password] != user_params[:password_confirmation]
+         flash[:error] = "Password and password confirmation do not match"
+         redirect_to register_user_path
+         return
+      end
+
       if user.save
          flash[:success] = 'Successfully Created New User'
          redirect_to user_path(user)
@@ -16,12 +23,13 @@ class UsersController < ApplicationController
          flash[:error] = "#{error_message(user.errors)}"
          redirect_to register_user_path
       end   
+      
    end
 
 private
 
   def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
