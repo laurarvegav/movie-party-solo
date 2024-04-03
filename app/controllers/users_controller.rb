94d624e9977bae5e974c86fb7802fb55eb031b58
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-03 16:46:51 -0600

    Refactor users controller removing login form and login actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,19 +26,6 @@ class UsersController < ApplicationController
       
    end
 
-   def login_form; end
-
-   def login
-      user = User.find_by(email: params[:email])
-      if user.authenticate(params[:password])
-         flash[:success] = "Welcome, #{user.name}!"
-         redirect_to user_path(user)
-      else
-         flash[:error] = "Sorry, incorrect credentials"
-         render :login_form
-      end
-   end
-
 private
   def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
