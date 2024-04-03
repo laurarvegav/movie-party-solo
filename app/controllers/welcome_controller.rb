e76d0a8a380befc11040f141eadbd7d34e2a1764
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-03 16:44:23 -0600

    Add cookie to greeting as practice excercise

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @users = User.all
+    unless cookies[:greeting]
+      cookies.encrypted[:greeting] = 'Hello there!'
+    end
   end
 end
