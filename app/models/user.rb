014828d9bada020da611c6c9bfc5966be4783313
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-03 15:39:03 -0600

    Add enum role

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
    validates_presence_of :password_digest
    has_secure_password
 
+   enum role: ["default", "manager", "admin"]
+
    has_many :user_parties
    has_many :viewing_parties, through: :user_parties
 end
