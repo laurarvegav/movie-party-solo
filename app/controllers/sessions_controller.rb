f4d87288260e7a2d2ad2f3631baf7c50ce9dd18e
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-04-03 16:47:31 -0600

    Implement cookies into admin and sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    session[:user_id] = user.id
+    cookies.encrypted[:location] = params[:location]
+    if user.authenticate(params[:password])
+      flash[:success] = "Welcome, #{user.name}!"
+      if user.admin?
+        redirect_to admin_dashboard_path
+      else
+        redirect_to user_path(user)
+      end
+    else
+      flash[:error] = "Sorry, incorrect credentials"
+      render :new
+    end
+  end
+
+  def destroy
+
+  end
+end
